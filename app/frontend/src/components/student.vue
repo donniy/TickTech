3a235bc0afd1fd90f2877b3a2251921c905e6ad0
Hazin Colak <hazin.colak@gmail.com>
2018-06-05 15:07:19 +0200

    student page with menu and css

--- app/frontend/src/components/student.vue
+++ app/frontend/src/components/student.vue
@@ -1,5 +1,35 @@
 <template>
     <div>
-        <h1>TickTech Student</h1>
+        <div class="vertical-menu">
+            <a href="#" class="active">Home</a>
+            <a href="#">My Tickets</a>
+            <a href="#">Submit a Ticket</a>
+            <a href="#">Archive</a>
+        </div>
     </div>
 </template>
+
+<style scoped>
+.vertical-menu {
+    float: left;
+    width: 200px; /* Set a width if you like */
+}
+
+.vertical-menu a {
+    background-color: #ffffff; /* Grey background color */
+    color: black; /* Black text color */
+    display: block; /* Make the links appear below each other */
+    padding: 12px; /* Add some padding */
+    text-decoration: none; /* Remove underline from links */
+    border-radius: 8px;
+}
+
+.vertical-menu a:hover {
+    background-color: #ccc; /* Dark grey background on mouse-over */
+}
+
+.vertical-menu a.active {
+    background-color: #CA0033; /* Add a green color to the "active/current" link */
+    color: white;
+}
+</style>
