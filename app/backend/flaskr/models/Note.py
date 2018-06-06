0297ab1e5333e486e994d88df3c9692161881758
Pieter <pieterdonkers_3@hotmail.com>
2018-06-06 15:52:40 +0200

    updated note editing

---
+++ app/backend/flaskr/models/Note.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from flaskr import database
+
+db = database.db
+
+class Note(db.Model):
+    """
+    Een notitie.
+    """
+    note_id = db.Column(db.Integer, primary_key=True)
+
+    ticket_id = db.Column(db.Integer, db.ForeignKey('ticket.id'), default=0, nullable=False)
+    message_id = db.Column(db.Integer, db.ForeignKey('message.message_id'), default=0,nullable=True)
+
+    user_id = db.Column(db.Integer, nullable=False)
+
+    text = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    ticket = db.relationship('Ticket', backref=db.backref('messages', lazy=True))
+    # message = db.relationship('Message', backref=db.backref('notes',lazy=True))
+
+    def __repr__(self):
+        return '<Note {}>'.format(self.note_id)
+
+    @property
+    def serialize(self):
+        return {
+            'note_id': self.note_id,
+            # 'message_id': self.message_id,
+            'ticket_id': self.ticket_id,
+            'user_id': self.user_id,
+            'text': self.text,
+            'timestamp': self.timestamp
+        }
