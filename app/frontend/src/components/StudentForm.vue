36f2df369e3981e5adad9ba3d3ea23f08d99573b
Rens van der Veldt <rensvanderveldt@gmail.com>
2018-06-06 11:54:58 +0200

    Added more backend communication to form and implementing course/category assoc

--- app/frontend/src/components/StudentForm.vue
+++ app/frontend/src/components/StudentForm.vue
@@ -31,17 +31,25 @@
                         </div>
 
                         <div class="form-group">
-                            <label for="category">Category</label>
-                            <select id="category" class="form-control custom-select" v-model="form.label_class">
+                            <label for="course">Course</label>
+                            <select id="course" class="form-control custom-select" v-model="form.label_class">
                                 <option disabled value="">Nothing selected</option>
-                                <option v-for="option in options.labels" v-bind:value="option.value">
+                                <option v-for="option in options.courses" v-bind:value="option.value">
                                 {{ option.text }}
                                 </option>
                             </select>
 
                         </div>
+                        <div class="form-group">
+                            <label for="category">Category</label>
+                            <select id="category" class="form-control custom-select" v-model="form.label_class">
+                                <option disabled value="">Nothing selected</option>
+                                <option v-for="option in options" v-bind:value="option.value">{{ option.text }}</option>
+                            </select>
 
-                        <button class="btn btn-primary">
+                        </div>
+
+                        <button class="btn btn-primary" v-bind:disabled="errors.any()">
                             Submit
                         </button>
 
@@ -67,19 +75,43 @@ export default {
                 label_class: "",
             },
             options: {
-                labels: [
-                    { value: "Assignment 1", text: "Ass1" },
-                    { value: "Assignment 2", text: "Ass2" },
-                    { value: "Assignment 3", text: "Ass3" },
-                    { value: "Deadlines", text: "Deadlines" },
-                    { value: "Absense", text: "Absense" },
-                    { value: "Course guide", text: "Course" }
-                ]
+                courses:[
+                    { value: "Prosoft", text: "Project software engineering"},
+                    { value: "ATF", text: "Automaten en formele talen"},
+                    { value: "OS", text: "Operating systems"}
+                ], labels: {
+                    Prosoft: [
+                        { value: "Ass1", text: "Assignment 1" },
+                        { value: "Ass2", text: "Assignment 2" },
+                        { value: "Ass3", text: "Assignment 3" },
+                        { value: "Deadlines", text: "Deadlines" },
+                        { value: "Absense", text: "Absense" },
+                        { value: "Course", text: "Course Guide" }
+                    ], ATF: [
+                        { value: "Ass1", text: "Klachten" },
+                        { value: "Ass2", text: "Meer klachten" },
+                        { value: "Ass3", text: "Klachten extra" },
+                        { value: "Deadlines", text: "Deadlines" },
+                        { value: "Absense", text: "Absense" },
+                        { value: "Course", text: "Course Guide" }
+                    ], OS: [
+                        { value: "Ass1", text: "Hackme1" },
+                        { value: "Ass2", text: "Schedulers" },
+                        { value: "Ass3", text: "Cijfer info" },
+                        { value: "Deadlines", text: "Deadlines" },
+                        { value: "Absense", text: "Absense" },
+                        { value: "Course", text: "Course Guide" }
+                    ]
+                }
             }
+        }
+    }, computed: {
+        options: function(event) {
+            return labels[this.category]
         }
     }, methods: {
         sendForm () {
-        const path = '/api/ticket/' + this.$route.params.ticket_id + '/reply'
+        const path = '/api/ticket/' + this.$route.params.ticket_id + '/submit'
         axios_csrf.post(path, {message: this.form})
         .then(response => {
             this.messages.push(response.data.message)
@@ -89,6 +121,9 @@ export default {
                 console.log(error)
             })
         }
+    }, onChange: function(e) {
+      console.log(event.srcElement.value);
+      this.options = this.options
     }
 }
 
