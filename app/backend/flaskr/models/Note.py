0cbfb8c2f3a8bd4707235e4faa36cb7e22eead64
Pieter <pieterdonkers_3@hotmail.com>
2018-06-07 15:17:15 +0200

    added support for notes

--- app/backend/flaskr/models/Note.py
+++ app/backend/flaskr/models/Note.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 from flaskr import database
+from sqlalchemy_utils import UUIDType
+import uuid
 
 db = database.db
 
@@ -7,9 +9,9 @@ class Note(db.Model):
     """
     Een notitie.
     """
-    note_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(UUIDType(binary=False), primary_key=True)
 
-    ticket_id = db.Column(db.Integer, db.ForeignKey('ticket.id'), default=0, nullable=False)
+    ticket_id = db.Column(UUIDType(binary=False),db.ForeignKey('ticket.id'), unique=False, nullable=True)
     # message_id = db.Column(db.Integer, db.ForeignKey('message.message_id'), default=0,nullable=True)
 
     user_id = db.Column(db.Integer, nullable=False)
@@ -17,16 +19,16 @@ class Note(db.Model):
     text = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-    ticket = db.relationship('Ticket', backref=db.backref('notes', lazy=True))
+    ticket = db.relationship('Ticket', backref=db.backref('notes', lazy=False))
     # message = db.relationship('Message', backref=db.backref('notes',lazy=True))
 
     def __repr__(self):
-        return '<Note {}>'.format(self.note_id)
+        return '<Note {}>'.format(self.id)
 
     @property
     def serialize(self):
         return {
-            'note_id': self.note_id,
+            'id': self.id,
             # 'message_id': self.message_id,
             'ticket_id': self.ticket_id,
             'user_id': self.user_id,
