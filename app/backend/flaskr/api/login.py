8834156c5609da9bb1fa51f95a3a9a9e17f53355
Jarno Bakker <jarnoaxelbakker@hotmail.com>
2018-06-08 13:43:28 +0200

    Login

--- app/backend/flaskr/api/login.py
+++ app/backend/flaskr/api/login.py
@@ -4,11 +4,12 @@ from flask_login import login_user
 from flaskr.models.User import *
 
 
-@apiBluePrint.route('/login', methods=['GET'])
+@apiBluePrint.route('/login', methods=['POST'])
 def login():
     """
     Kijk of de login gegevens in POST correct zijn/communiceer met Canvas.
     """
     user_id = request.json.get('user_id')
     user = User.query.get(user_id)
-    return jsonify({'status': "success", 'message': {'text': request.json.get("message"), 'user_id': 12345678, 'id': 5}})
+
+    return jsonify({'status': 'success', 'user': user.serialize})
