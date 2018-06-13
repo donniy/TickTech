b36af2e516817b5e8bb81a7f41b4ebf9bc137077
Rens van der Veldt <rensvanderveldt@gmail.com>
2018-06-13 13:13:43 +0200

    beautified code

--- app/frontend/src/components/Message.vue
+++ app/frontend/src/components/Message.vue
@@ -1,26 +1,24 @@
 <template>
     <div :class="'message' + (user.id == message.user_id ? ' text-right' : '')">
-        <h4 class="message-sender">{{username}}</h4>
-        <div class="material-card">
-            <p class="message-text">
-            {{message.text}}
-            </p>
-        </div>
+    	<h4 class="message-sender">{{username}}</h4>
+    	<div class="material-card">
+    		<p class="message-text">
+    			{{message.text}}
+    		</p>
+    	</div>
     </div>
 </template>
 
 <script>
-
-export default {
-    props: ['message', 'user'],
-    computed: {
-        username: function () {
-            return this.user.id == this.message.user_id ? 'You' : this.message.user_id
-        },
-    },
-    data: function () {
-        return {};
-    },
-}
-
+    export default {
+    	props: ['message', 'user'],
+    	computed: {
+    		username: function() {
+    			return this.user.id == this.message.user_id ? 'You' : this.message.user_id
+    		},
+    	},
+    	data: function() {
+    		return {};
+    	},
+    }
 </script>
--- app/frontend/src/components/TA_Inbox.vue
+++ app/frontend/src/components/TA_Inbox.vue
@@ -1,81 +1,75 @@
 <template>
     <div>
-        <h1>Welkom {{ta.name}}</h1>
+    	<h1>Welkom {{ta.name}}</h1>
 
 
-        <!-- <b-table striped hover :items="cases">
-            <template slot="id" slot-scope="data">
-                <a v-bind:href="'/ticket/'+data.value">
-                {{data.value}}
-                </a>
-            </template>
-        </b-table> -->
-        <course
-          v-for="course in courses"
-          v-bind:key="course.id"
-          v-bind:course="course"
-        ></course>
+    	<!-- <b-table striped hover :items="cases">
+                <template slot="id" slot-scope="data">
+                    <a v-bind:href="'/ticket/'+data.value">
+                    {{data.value}}
+                    </a>
+                </template>
+            </b-table> -->
+    	<course v-for="course in courses" v-bind:key="course.id" v-bind:course="course"></course>
     </div>
 </template>
 
 <script>
-
 import axios from 'axios'
 import Course from './Course.vue'
 
 export default {
-    data () {
-        return {
-            ta: {},
-            cases: [],
-            courses: []
+	data() {
+		return {
+			ta: {},
+			cases: [],
+			courses: []
 
-        }
-    },
-    methods: {
-        getTA () {
-            const path = '/api/user/' + this.$route.params.ta_id
-            axios.get(path)
-            .then(response => {
-                this.ta = response.data.json_data
-                console.log(this.ta)
-            })
-            .catch(error => {
-                console.log(error)
-            })
-        },
+		}
+	},
+	methods: {
+		getTA() {
+			const path = '/api/user/' + this.$route.params.ta_id
+			axios.get(path)
+				.then(response => {
+					this.ta = response.data.json_data
+					console.log(this.ta)
+				})
+				.catch(error => {
+					console.log(error)
+				})
+		},
 
-        getAllCases () {
-            const path = '/api/ta'
-            axios.get(path)
-            .then(response => {
-                this.cases = response.data
-            })
-            .catch(error => {
-                console.log(error)
-            })
-        },
-        getTaCourses () {
-            axios.get('/api/ta/'+ this.$route.params.ta_id + '/courses')
-            .then(response => {
-                this.courses = response.data.json_data
-                console.log(this.courses)
-            }).catch(error => {
-                console.log(error)
-            })
-        },
-        created () {
-            this.getTA()
-            this.getAllCases()
-            this.getTaCourses()
-        }
-    },
-    mounted: function () {
-        this.created()
-    },
-    components: {
-        'course': Course,
-    }
+		getAllCases() {
+			const path = '/api/ta'
+			axios.get(path)
+				.then(response => {
+					this.cases = response.data
+				})
+				.catch(error => {
+					console.log(error)
+				})
+		},
+		getTaCourses() {
+			axios.get('/api/ta/' + this.$route.params.ta_id + '/courses')
+				.then(response => {
+					this.courses = response.data.json_data
+					console.log(this.courses)
+				}).catch(error => {
+					console.log(error)
+				})
+		},
+		created() {
+			this.getTA()
+			this.getAllCases()
+			this.getTaCourses()
+		}
+	},
+	mounted: function() {
+		this.created()
+	},
+	components: {
+		'course': Course,
+	}
 }
-
 </script>
