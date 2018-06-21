3d8694e68975db574a0a77d95f9b0b0b3c360e8a
Rens van der Veldt <rensvanderveldt@gmail.com>
2018-06-22 00:57:16 +0200

    final fix for teampage button and getstarted linking to 404 fixed when logged in

--- app/frontend/src/components/Home.vue
+++ app/frontend/src/components/Home.vue
@@ -100,7 +100,7 @@
 					</div>
 				</div>
 				<div class="middle-display">
-					<router-link to="/login" :class="'button text-uppercase scroll-button btn-arrow'">
+					<router-link to="/home" :class="'button text-uppercase scroll-button btn-arrow'">
 						<span>Get Started Now!
 							<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 							 viewBox="0 0 36.1 25.8" enable-background="new 0 0 36.1 25.8" xml:space="preserve">
