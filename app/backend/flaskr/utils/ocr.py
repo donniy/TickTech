0ce8a939f2434f6ef58128b4bee2597f595479ca
Donald Gaga <donalddedii@gmail.com>
2018-06-21 12:15:16 +0200

    fixed buggs

--- app/backend/flaskr/utils/ocr.py
+++ app/backend/flaskr/utils/ocr.py
@@ -14,7 +14,7 @@ def ocr_file(image_path):
     #image = cv2.resize(img, (0,0), fx=2, fy=2) 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # Apply gausian blur to remove noise
-    gray = cv2.GaussianBlur(gray,(5,5),0)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
 
     # write the grayscale image to disk as a temporary file so we can
     # apply OCR to it
@@ -24,9 +24,10 @@ def ocr_file(image_path):
     # create textfile, load the image as a PIL/Pillow image, apply OCR, 
     # write to created textfile,and then delete the temporary file
     textfile = open("{}.txt".format(os.getpid()), 'w')
-    # -load_freq_dawg=False -load_system_dawg=False -preserve_interword_spaces=1 -psm=5
+    # -load_freq_dawg=False -load_system_dawg=False -preserve_interword_spaces=1
+    # -psm=5
     # config='--preserve_interword_spaces 1',
-    text = pytesseract.image_to_string(Image.open(filename),lang='eng', nice=1)
+    text = pytesseract.image_to_string(Image.open(filename), lang='eng', nice=1)
     os.remove(filename)
     textfile.write(text)
     textfile.close()
