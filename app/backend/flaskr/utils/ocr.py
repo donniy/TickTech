6ac5ae188a32b305a41a0dc317949a52b07e8c1e
Donald Gaga <donalddedii@gmail.com>
2018-06-24 14:32:15 +0200

    ocr added, busy with requests

--- app/backend/flaskr/utils/ocr.py
+++ app/backend/flaskr/utils/ocr.py
@@ -7,10 +7,11 @@ from StringIO import StringIO
 
 def process_image(url):
     image = _get_image(url)
-    image.convert('1')
+    #image.convert('1')
     image.filter(ImageFilter.GaussianBlur(2))
     image.filter(ImageFilter.SHARPEN)
     return pytesseract.image_to_string(image, lang='eng')
+
 
 def _get_image(url):
     return Image.open(StringIO(requests.get(url).content))
