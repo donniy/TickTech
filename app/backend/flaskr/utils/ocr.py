1511c0c36e1eaf453255ac3df6e0cb72dd47ac78
Donald Gaga <donalddedii@gmail.com>
2018-06-24 14:15:22 +0200

    ocr added, busy with requests

--- app/backend/flaskr/utils/ocr.py
+++ app/backend/flaskr/utils/ocr.py
@@ -1,35 +1,16 @@
-# import the necessary packages
+import pytesseract
+import requests
 from PIL import Image
 from PIL import ImageFilter
 from StringIO import StringIO
-import pytesseract
-import argparse
-import requests
-import cv2
-import os
 
 
-def ocr_file(image_path):
-    # load the example image and convert it to grayscale
-    img = cv2.imread(image_path)
-    # image = cv2.resize(img, (0,0), fx=2, fy=2)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # Apply gausian blur to remove noise
-    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+def process_image(url):
+    image = _get_image(url)
+    image.convert('1')
+    image.filter(ImageFilter.GaussianBlur(2))
+    image.filter(ImageFilter.SHARPEN)
+    return pytesseract.image_to_string(image, lang='eng')
 
-    # write the grayscale image to disk as a temporary file so we can
-    # apply OCR to it
-    filename = "{}.png".format(os.getpid())
-    cv2.imwrite(filename, gray)
-
-    # create textfile, load the image as a PIL/Pillow image, apply OCR,
-    # write to created textfile,and then delete the temporary file
-    textfile = open("{}.txt".format(os.getpid()), 'w')
-    # -load_freq_dawg=False -load_system_dawg=False-preserve_interword_spaces=1
-    # -psm=5
-    # config='--preserve_interword_spaces 1',
-    text = pytesseract.image_to_string(Image.open(filename), lang='eng',
-                                       nice=1)
-    os.remove(filename)
-    textfile.write(text)
-    textfile.close()
+def _get_image(url):
+    return Image.open(StringIO(requests.get(url).content))
