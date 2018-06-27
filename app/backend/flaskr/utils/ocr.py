23f48f49acd1295f6224a97eabc86c75e6a80469
Donald Gaga <donalddedii@gmail.com>
2018-06-27 10:46:11 +0200

    ocr file changed

--- app/backend/flaskr/utils/ocr.py
+++ app/backend/flaskr/utils/ocr.py
@@ -8,7 +8,7 @@ def ocr_process_image(url):
     try:
         image = cv2.imread(url)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        gray = cv2.threshold(gray, 0, 255, 
+        gray = cv2.threshold(gray, 0, 255,
                              cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
         return pytesseract.image_to_string(gray, lang='eng')
     except IOError:
